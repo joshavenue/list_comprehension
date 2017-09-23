62e400718983ea05cf63b4502abf3eb03ce44259
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-23 22:23:48 +0800

    Create example_1.py

---
+++ example_1.py
@@ -0,0 +1,7 @@
+x = [i**3 for i in range(5)]
+
+print(x)
+
+## Creates a list where the range is 0 till 5, which is [0,1,2,3,4],
+## Whereby i = 0,1,2,3,4 is multiplied in the power of 3
+## Hence the outcome will be : [0, 1, 8, 27, 64]
